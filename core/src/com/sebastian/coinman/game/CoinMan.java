b33650c37f6b90cc8b75679c8d4188fab73e85e8
Sebastian Lundquist <sebastian.lundquist@gmail.com>
2019-09-29 10:36:03 +0200

    Add collision detection

--- core/src/com/sebastian/coinman/game/CoinMan.java
+++ core/src/com/sebastian/coinman/game/CoinMan.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Rectangle;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -20,16 +22,19 @@ public class CoinMan extends ApplicationAdapter {
 	float gravity = 2f;
 	float velocity = 0;
 	int manY;
+	Rectangle manRectangle;
 
 	Random random;
 
 	ArrayList<Integer> coinXs = new ArrayList<>();
 	ArrayList<Integer> coinYs = new ArrayList<>();
+	ArrayList<Rectangle> coinRectangles = new ArrayList<>();
 	Texture coin;
 	int coinCount;
 
 	ArrayList<Integer> bombXs = new ArrayList<>();
 	ArrayList<Integer> bombYs = new ArrayList<>();
+	ArrayList<Rectangle> bombRectangles = new ArrayList<>();
 	Texture bomb;
 	int bombCount;
 
@@ -76,9 +81,11 @@ public class CoinMan extends ApplicationAdapter {
 			makeBomb();
 		}
 
+		bombRectangles.clear();
 		for (int i = 0; i < bombXs.size(); i++) {
 			batch.draw(bomb, bombXs.get(i), bombYs.get(i));
 			bombXs.set(i, bombXs.get(i) - 15);
+			bombRectangles.add(new Rectangle(bombXs.get(i), bombYs.get(i), bomb.getWidth(), bomb.getHeight()));
 		}
 
 		if (coinCount < 100) {
@@ -89,9 +96,11 @@ public class CoinMan extends ApplicationAdapter {
 			makeCoin();
 		}
 
+		coinRectangles.clear();
 		for (int i = 0; i < coinXs.size(); i++) {
 			batch.draw(coin, coinXs.get(i), coinYs.get(i));
 			coinXs.set(i, coinXs.get(i) - 20);
+			coinRectangles.add(new Rectangle(coinXs.get(i), coinYs.get(i), coin.getWidth(), coin.getHeight()));
 		}
 
 		if (Gdx.input.justTouched()) {
@@ -119,6 +128,17 @@ public class CoinMan extends ApplicationAdapter {
 		}
 
 		batch.draw(man[manState], width / 4 - man[0].getWidth() / 2, manY);
+		manRectangle = new Rectangle(width / 4 - man[0].getWidth() / 2, manY, man[manState].getWidth(), man[manState].getHeight());
+		for (int i = 0; i < coinRectangles.size(); i++) {
+			if (Intersector.overlaps(manRectangle, coinRectangles.get(i))) {
+				Gdx.app.log("Coin", "Collided");
+			}
+		}
+		for (int i = 0; i < bombRectangles.size(); i++) {
+			if (Intersector.overlaps(manRectangle, bombRectangles.get(i))) {
+				Gdx.app.log("Bomb", "Collided");
+			}
+		}
 		batch.end();
 	}
 	
